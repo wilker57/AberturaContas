7171fdba93b4a7e617112fe82ed7903ba9f23680
wilker57 <developer@aberturacontas.local>
2025-11-09 15:23:06 -0400

    Estruturação de consultas sql

--- produto/views.py
+++ produto/views.py
@@ -343,13 +343,9 @@ def agencias():
     
     query = """
         SELECT *,
-        (SELECT nome FROM banco WHERE banco.id_banco = agencia.id_banco) AS banco_nome
-        FROM agencia
-<<<<<<< HEAD
-        ORDER BY nome_agencia;
-=======
+        (SELECT nome FROM banco WHERE banco.id_banco = a.id_banco) AS banco_nome
+        FROM agencia a
         ORDER BY a.nome_agencia;
->>>>>>> cc6496e378aa030fad768eface98cc66458af699
     """
     agencias_list = execute_query(query)
     
@@ -653,20 +649,24 @@ def remessas():
         return redirect(url_for('views.login'))
     
     query = """
-        SELECT *,
-<<<<<<< HEAD
-=======
-        (SELECT nome_agencia FROM agencia WHERE agencia.id_banco = remessa.id_banco) AS nome_agencia,
->>>>>>> cc6496e378aa030fad768eface98cc66458af699
-        (SELECT nome FROM concedente WHERE concedente.id_concedente = remessa.id_concedente) AS concedente_nome,
-        (SELECT nome FROM usuario WHERE usuario.id_usuario = remessa.id_usuario) AS usuario_nome,
-        (SELECT nome FROM banco WHERE banco.id_banco = remessa.id_banco) AS banco_nome
-        FROM remessa
-        ORDER BY dt_remessa DESC;
+        SELECT
+            r.*,
+            ag.nome_agencia,
+            c.nome AS concedente_nome,
+            u.nome AS usuario_nome,
+            b.nome AS banco_nome
+        FROM remessa r
+        LEFT JOIN conta_convenio cc ON r.id_remessa = cc.id_remessa
+        LEFT JOIN agencia ag ON cc.id_agencia = ag.id_agencia
+        LEFT JOIN concedente c ON r.id_concedente = c.id_concedente
+        LEFT JOIN usuario u ON r.id_usuario = u.id_usuario
+        LEFT JOIN banco b ON r.id_banco = b.id_banco
+        ORDER BY r.dt_remessa DESC;
     """ 
     remessas_list = execute_query(query)
     
     return render_template('remessas/list.html', remessas=remessas_list)
+
 
 @views_bp.route('/remessas/criar', methods=['GET', 'POST'])
 def criar_remessa():
@@ -944,11 +944,7 @@ def editar_conta_convenio(id_conta_convenio):
         SELECT *,
         (SELECT num_processo FROM remessa WHERE remessa.id_remessa = cc.id_remessa) AS num_processo,
         (SELECT nome_agencia FROM agencia WHERE agencia.id_agencia = cc.id_agencia) AS nome_agencia,
-<<<<<<< HEAD
         (SELECT nome FROM banco WHERE banco.id_banco = (SELECT id_banco FROM agencia WHERE agencia.id_agencia = cc.id_agencia)) AS banco_nome
-=======
-        (SELECT nome FROM banco WHERE banco.id_banco = (SELECT id_banco FROM agencia WHERE agencia.id_agencia = cc.id_agencia) AS banco_nome
->>>>>>> cc6496e378aa030fad768eface98cc66458af699
         FROM conta_convenio cc
         WHERE cc.id_conta_convenio = %s
     """
